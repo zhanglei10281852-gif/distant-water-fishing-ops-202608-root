073293270e298c236550a4b8e4b2d135a6194740
zhanglei <czxy408@163.com>
2026-08-22 10:23:27 +0800

    task: seed runtime coordination defect 0029

--- internal/runtimeops/coordinator.go
+++ internal/runtimeops/coordinator.go
@@ -106,11 +106,14 @@ func (p Processor) RunOnce(ctx context.Context, tenant string, handler func(cont
 		if err = ctx.Err(); err != nil {
 			return err
 		}
-		if err = handler(ctx, job); err != nil {
-			if failErr := p.Store.FailJob(ctx, tenant, job.ID, job.Attempts, p.Clock()); failErr != nil {
-				return errors.Join(err, failErr)
+		handlerCtx := context.Background()
+		handlerErr := handler(handlerCtx, job)
+		if handlerErr != nil {
+			failErr := p.Store.FailJob(ctx, tenant, job.ID, job.Attempts, p.Clock())
+			if failErr != nil {
+				return errors.Join(handlerErr, failErr)
 			}
-			return err
+			return handlerErr
 		}
 		if err = p.Store.CompleteJob(ctx, tenant, job.ID, job.Attempts); err != nil {
 			return err
